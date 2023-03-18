d92da4ad2bf46da4903fec2c6504750df426644e
lightsilverberryfox <118631656+lightsilverberryfox@users.noreply.github.com>
2023-03-18 13:15:52 +0000

    Update linux_build.sh

--- linux_build.sh
+++ linux_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt install gcc g++ cmake libjpeg-dev libpng-dev libtiff5 libtiff5-dev libboost-test-dev qtbase5-dev libqt5svg5-dev qttools5-dev qttools5-dev-tools libqt5opengl5-dev
+sudo apt install gcc g++ cmake libjpeg-dev libpng-dev libtiff5 libtiff5-dev libboost-test-dev qtbase5-dev libqt5svg5-dev qttools5-dev qttools5-dev-tools libqt5opengl5-dev
 
 mkdir build; cd build
 cmake -G "Unix Makefiles" --build ..
