9226f59653a3ab0d7caae5038255a5c1e2e60043
lightsilverberryfox <118631656+lightsilverberryfox@users.noreply.github.com>
2023-03-18 13:13:26 +0000

    Create linux_build.sh

---
+++ linux_build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+apt install gcc g++ cmake libjpeg-dev libpng-dev libtiff5 libtiff5-dev libboost-test-dev qtbase5-dev libqt5svg5-dev qttools5-dev qttools5-dev-tools libqt5opengl5-dev
+
+mkdir build; cd build
+cmake -G "Unix Makefiles" --build ..
+make -j `nproc`
